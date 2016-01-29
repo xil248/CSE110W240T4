519843653d5b16f59cb611edbc343582e67d08f6
Shuyu Gu <shuyugu@Shuyus-MacBook-Pro.local>
2016-01-28 21:19:02 -0800

    minor change2

---
+++ TeamUpTeamUp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TeamUp
+//
+//  Created by Reno & Jenny on 1/26/16.
+//  Copyright © 2016 CSE110W240T4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TeamUpTeamUp/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  TeamUp
+//
+//  Created by Reno & Jenny on 1/26/16.
+//  Copyright © 2016 CSE110W240T4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *emailText;
+@property (weak, nonatomic) IBOutlet UITextField *passwordText;
+@property (weak, nonatomic) IBOutlet UITextField *enterEmailText;
+@property (weak, nonatomic) IBOutlet UITextField *enterPasswordText;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordText;
+@property (weak, nonatomic) IBOutlet UITextField *gsy;
+
+
+@end
+
