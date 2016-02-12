74d3eec44899577da13ede3e35e7ce67f1c8a227
renoyin <renoyin9536@gmail.com>
2016-02-11 17:02:27 -0800

    change database path

--- TeamUp/TeamUp/MessagesViewController.swift
+++ TeamUp/TeamUp/MessagesViewController.swift
@@ -28,7 +28,7 @@ class MessagesViewController: JSQMessagesViewController {
   
   func setupFirebase() {
     // *** STEP 2: SETUP FIREBASE
-    messagesRef = Firebase(url: "https://swift-chat.firebaseio.com/messages")
+    messagesRef = Firebase(url: "https://resplendent-inferno-8485.firebaseio.com/messages")
     
     // *** STEP 4: RECEIVE MESSAGES FROM FIREBASE (limited to latest 25 messages)
     messagesRef.queryLimitedToNumberOfChildren(25).observeEventType(FEventType.ChildAdded, withBlock: { (snapshot) in
